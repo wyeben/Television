b0e99e438de43e5cea27b0eede617d26ec740c41
Yila Benson <134516892+wyeben@users.noreply.github.com>
2023-06-04 09:28:05 +0100

    Add files via upload

---
+++ Television.java
@@ -0,0 +1,35 @@
+ class Television{
+
+   private boolean Power;
+   
+   private int Volume;
+   
+   private int Channel;
+   
+   
+   
+      public void setPower(boolean power){
+        this.Power = Power;
+         }
+         
+     public boolean getPower(){
+      return Power = true;
+     }
+     
+     public void setVolume(int volume){
+      this.Volume = Volume;
+     }
+     
+     public int getVolume(){
+      return Volume = 37;
+     }
+     
+     public void setChannel(int channel){
+       this.Channel = Channel;
+    }
+     public int getChannel(){
+        return Channel = 5;
+     }
+     
+      }
+
